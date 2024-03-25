b1cf5a2f67dcef7043f24942d188f81681710197
Toluthedev <enochjesse884@gmail.com>
2024-03-25 09:13:38 +0100

    Add Facility model and update Room model with facility field

---
+++ giga_hotel_main/src/models/facility.model.ts
@@ -0,0 +1,18 @@
+import mongoose , { Document, Schema } from 'mongoose';
+
+interface IFacility extends Document {
+    name: string;
+    description: string;
+}
+
+interface IFacilityModel extends mongoose.Model<IFacility> {
+}
+
+const FacilitySchema: Schema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true }
+});
+
+const Facility: IFacilityModel = mongoose.model<IFacility, IFacilityModel>('Facility', FacilitySchema);
+
+export default Facility;
--- giga_hotel_main/src/models/room.model.ts
+++ giga_hotel_main/src/models/room.model.ts
@@ -1,21 +1,24 @@
 import mongoose, { Document, Schema } from 'mongoose';
+import Facility from './facility.model';
 
 interface IRoom extends Document {
     roomNumber: number;
     type: 'regular' | 'premium' | 'luxury';
     isAvailable: boolean;
     price: number;
+    facility: Schema.Types.ObjectId[];
+    roomCapacity: number;
 }
 
 const RoomSchema: Schema = new Schema({
     roomNumber: { type: Number, required: true, unique: true },
     type: { type: String, required: true, enum: ['regular', 'premium', 'luxury'] },
     isAvailable: { type: Boolean, default: true },
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    facility: [{ type: Schema.Types.ObjectId, ref: 'Facility' },  { default: [] }],
+    roomCapacity: { type: Number, required: true }
 });
 
 const Room = mongoose.model<IRoom>('Room', RoomSchema);
 
 export { Room, IRoom };
-
-//still working on this 
